4a8ddcfa0ed4c4b8f40936f6483051bf373353e6
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-10 22:26:03 -0600

    se agregaron metodos para cadenas

--- basic.js
+++ basic.js
@@ -14,3 +14,9 @@ let texto="there something inside you its hard to explain";
 //acceder a la longitud de un string
 console.log('propedad length',texto.length);
 
+//formas de acceder a un caracter de una cadena
+
+console.log('Por .at', texto.at(10));
+console.log('Por .charAt', texto.charAt(8));
+console.log('Por .chartCodeAt', texto.charCodeAt(7));
+console.log('Por su indice [i]', texto[0]);
