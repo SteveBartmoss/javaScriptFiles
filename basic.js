0fe42bd95a520360b05be62cc161b58d5f336938
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-10 22:45:54 -0600

    se agregaron metodos para dividir una cadena

--- basic.js
+++ basic.js
@@ -20,3 +20,11 @@ console.log('Por .at', texto.at(10));
 console.log('Por .charAt', texto.charAt(8));
 console.log('Por .chartCodeAt', texto.charCodeAt(7));
 console.log('Por su indice [i]', texto[0]);
+
+//formas de dividir un string 
+
+let midleSlice=texto.slice(0,10);
+let midleSubString=texto.substring(10,20);
+
+console.log('Con slice', midleSlice);
+console.log('Con subString', midleSubString);
