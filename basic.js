ac507067ed2e80f879966685fd890f96468a483a
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-10 22:14:24 -0600

    se agrego contenido de cadenas

--- basic.js
+++ basic.js
@@ -5,4 +5,12 @@ let tiempo=10;
 let altura=1.80;
 let nombre="Steve";
 
+// asi se pueden imprimr multiples variables
 console.log('tiempo',tiempo,'altura',altura,'nombre',nombre);
+
+//metodos de strings
+let texto="there something inside you its hard to explain";
+
+//acceder a la longitud de un string
+console.log('propedad length',texto.length);
+
