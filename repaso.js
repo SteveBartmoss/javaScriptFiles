faba6ad21befa4c7c5e8db605ed9f07ed4700857
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-12 13:38:26 -0600

    se agrego la seccion de let vs var vs const

---
+++ repaso.js
@@ -0,0 +1,14 @@
+//let vs var vs const
+
+//no usar var porque es viejo
+let nombre='Steve'; //usar let cuando se que el valor puede cambiar
+const apellido='Wozniak'; //usar const cuando el valor no cambia
+
+console.log(nombre,apellido);
+
+if(true){
+    let nombre='Peter'; //aqui encuentra el scope de la variable
+    console.log(nombre); //esta varieable solo existe dentro del if
+}
+
+console.log(nombre);
