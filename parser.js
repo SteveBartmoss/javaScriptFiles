fb6ae05299273b6378b96dd3259e423f542e2b91
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-14 21:07:30 -0600

    se creo un prototipo de un parser para el proyecto de resaltado de sintaxis

---
+++ parser.js
@@ -0,0 +1,34 @@
+const code= `
+  import { NavLink } from "react-router-dom" 
+  import { DivCol, DivFooterContent, DivMainContent, DivRow } from "../componentes/contenedores"
+  export function Foter() {
+    return (
+        <div className="div-footer">
+            <DivMainContent>
+                <DivFooterContent>
+                    <DivRow>
+                        <p className="footer-link">Power by DevFarming</p>
+                    </DivRow>
+                    <DivRow>
+                        <NavLink className="footer-link" to="/news">Que hay de nuevo</NavLink>
+                        <NavLink className="footer-link" to="/about">About</NavLink>
+                        <p className="footer-link">Licencia</p>
+                        <NavLink className="footer-link" to="/documentation">Documentacion</NavLink>
+                    </DivRow>
+                </DivFooterContent>
+            </DivMainContent>
+        </div>
+    );
+}` 
+
+const leters=/[a-zA-z]/
+const simbols=/[{},."()-/;]/
+
+for(let i=0; i<code.length; i++){
+    if(leters.test(code[i])){
+        console.log(code[i])
+    }
+    if(simbols.test(code[i])){
+        console.log(code[i])
+    }
+}
