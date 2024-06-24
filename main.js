3b01e5320fce1775ae4cdbfa3ae6b5a95e7219ad
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-23 19:52:36 -0600

    se agrego codigo para probar las funciones

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-import { generateUniqueId } from "./generateUid.js";
+import { generateUniqueId, generateUniqueIdV2, generateUniqueIdV3 } from "./generateUid.js";
 
 for (let ite=0; ite<10; ite++){
     console.log(generateUniqueId())
 }
+console.log('------------------')
+
+for (let ite=0; ite<10; ite++){
+    console.log(generateUniqueIdV2())
+}
+console.log('------------------')
+/*for (let ite=0; ite<10; ite++){
+    console.log(generateUniqueIdV3())
+    console.log('------------------')
+}*/
