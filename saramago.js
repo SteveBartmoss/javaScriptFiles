ac28f762d19d4318eb95fb47415d0c1e645e1391
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-06 12:45:53 -0600

    se corrigeron las expresiones regulares y se mejoro el decorado de lso tokens

--- saramago.js
+++ saramago.js
@@ -1,14 +1,28 @@
 
 
-function tokenDecorator(token){
+export function tokenDecorator(token){
     let labelClose= /<\/[a-zA-Z][a-zA-Z0-9\-]*>/
 
-    let labelOpen= / <[a-zA-Z][a-zA-Z0-9\-]*/
+    let labelOpen= /<[a-zA-Z][a-zA-Z0-9\-]*/
 
     let reservedWords=['className','id','onClick'];
 
+    let operators=['+','-','=','*','&','%']
+
+    let codeDividers=['[',']','{','}','(',')']
+
+    let decoredToken
+
+    if(token === '>'){
+        decoredToken={
+            rawToken: token,
+            typeToken: 'label'
+        }
+        return decoredToken
+    }
+
     if(labelClose.test(token)){
-        let decoredToken={
+        decoredToken={
             rawToken: token,
             typeToken: 'label'
         }
@@ -16,19 +30,40 @@ function tokenDecorator(token){
     }
 
     if(labelOpen.test(token)){
-        let decoredToken={
+        decoredToken={
             rawToken: token,
             typeToken: 'label'
         }
         return decoredToken
     }
 
+    if(operators.includes(token)){
+        decoredToken={
+            rawToken: token,
+            typeToken: 'operator'
+        }
+        return decoredToken
+    }
+
+    if(codeDividers.includes(token)){
+        decoredToken={
+            rawToken: token,
+            typeToken: 'divider'
+        }
+        return decoredToken
+    }
+
     if(reservedWords.includes(token)){
-        let decoredToken={
+        decoredToken={
             rawToken: token,
             typeToken: 'reservedWord'
         }
         return decoredToken
+    }else{
+        decoredToken={
+            rawToken: token,
+            typeToken: 'text'
+        }
     }
 
 }
