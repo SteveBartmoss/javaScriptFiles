afd68c1160668e8eccc9818939e3e0ffcd5500b3
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-16 21:24:04 -0600

    se agrego un repaso de funciones

---
+++ functions.js
@@ -0,0 +1,34 @@
+
+
+//expresion como una funcion 
+
+let sayHi = function(){
+    alert("hola");
+}
+
+function sayHiFunc(){
+    alert('Hola')
+}
+
+alert(sayHiFunc) // esto no ejecuta la funcion pues faltal los parentesis
+
+let varFunction=sayHiFunc
+
+varFunction() //esto tambien puede invocar la funcion ya que se previamente se paso la referencia a una funcion
+
+
+//paso de una funcion como paramaetro que se ejecuta dependiendo de la accion 
+function ask(question, yes, no){
+    if(confirm(question)) yes()
+    else no()
+}
+
+function showOk(){
+    alert("Estas de acuerdo")
+}
+
+function showCancel(){
+    alert("Cancelaste la ejecucion")
+}
+
+ask("Estas de acuerdo?",showOk, showCancel)
